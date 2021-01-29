477fd5795fb02aff9c0520f55428519eeb842db7
Нуров Джовид <jovid.nurov@megafontj.tj>
2021-01-29 10:39:42 +0500

    readme should be added

---
+++ server_reload.py
@@ -0,0 +1,34 @@
+import telnetlib
+import time
+import json
+
+#in this varieble we have our servers info(hosts, and ports)
+payload = '[{"ip":"10.200.110.10", "port": "1101"},\
+            {"ip":"10.200.110.10", "port": "1101"},\
+            {"ip":"10.200.110.10", "port": "1101"},\
+            {"ip":"10.200.110.10", "port": "1101"},\
+            {"ip":"10.200.110.10", "port": "1101"},\
+            {"ip":"10.200.110.10", "port": "1101"}\
+          ]' 
+
+def reload_telnet(ip_address, port, command):
+    tn = telnetlib.Telnet(ip_address, port)
+    tn.write(command.encode('ascii') + b"\n") # means pressing ENTER  
+    time.sleep(1)
+
+json_payload = json.loads(payload)
+
+action = int(input("Command1 --> 1  Command2, bis_subs2 --> 2: "))
+print(action)
+
+for i in json_payload:
+    reload_telnet(i["ip"], i["port"], "command") #follow out command
+    reload_telnet(i["ip"], i["port"], "quit")    #then follow quit
+    print(i["ip"] + ' ' + i["port"] + " command " + "Done!")
+
+    if action == 2:
+        reload_telnet(i["ip"], i["port"], "command")
+        reload_telnet(i["ip"], i["port"], "quit")
+        print(i["ip"] + ' ' + i["port"] + " command " + "Done!")
+
+
